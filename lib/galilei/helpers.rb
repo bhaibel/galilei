07269af971f40926d7a8cf04a6c869844fa7f210
Betsy Haibel <betsy.haibel@gmail.com>
2011-04-13 23:13:05 -0400

    adding in ranges, symbols as ways of specifying length

--- lib/galilei/helpers.rb
+++ lib/galilei/helpers.rb
@@ -2,6 +2,26 @@ module Galilei
   module Helpers
     def method_missing(method_name, *args)
       length = args.shift
+      
+      # length can be a symbol, a range, an integer, or a fixnum. In the former two cases we need to convert it to something faker can use.
+      
+      # so first we translate symbols to ranges
+      length = case length
+      when :short
+        (1..5)
+      when :medium
+        (6..12)
+      when :long
+        (13..22)
+      else
+        length
+      end
+      
+      #then we translate ranges to actual numbers
+      if length.class == Range
+        length = rand(length.last - length.first) + length.first
+      end
+      
       Faker.constants.each do |faker_name|
         faker = Faker.const_get(faker_name)
         if faker.respond_to? method_name
