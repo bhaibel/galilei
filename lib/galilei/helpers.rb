bca9bd8657805a39ed2de75f666fa75b9aa0735c
Betsy Haibel <betsy.haibel@gmail.com>
2011-04-13 22:52:16 -0400

    allowing for varying lengths

--- lib/galilei/helpers.rb
+++ lib/galilei/helpers.rb
@@ -1,10 +1,15 @@
 module Galilei
   module Helpers
-    def method_missing(method_name)
+    def method_missing(method_name, *args)
+      length = args.shift
       Faker.constants.each do |faker_name|
         faker = Faker.const_get(faker_name)
         if faker.respond_to? method_name
-          return faker.send method_name
+          if length
+            return faker.send method_name, length
+          else
+            return faker.send method_name
+          end
         end
       end
     end
