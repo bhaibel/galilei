119f383c7d1f9a104a697c8dba8a7a1552d791a2
Betsy Haibel <betsy.haibel@gmail.com>
2011-04-13 21:43:59 -0400

    have our two main routes working

--- config.ru
+++ config.ru
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sinatra'
+require 'haml'
 
 require 'environment.rb'
 require 'lib/galilei.rb'
--- lib/galilei.rb
+++ lib/galilei.rb
@@ -2,9 +2,23 @@ require File.join(File.dirname(__FILE__), '..', 'environment.rb')
 require 'sinatra/base'
 
 class Galilei::App < Sinatra::Base
+  STORAGE_DIR = File.join(File.dirname(__FILE__), '..', 'storage')
+  
   set :app_file, __FILE__
+  set :views, STORAGE_DIR
   
   get '/' do
-    "Hello world!"
+    Dir.new(STORAGE_DIR).inject("") do |response, filename|
+      if filename[0,1] == '.'
+        response
+      else
+        response << "<a href='#{filename}'>#{filename}</a>"
+      end
+    end
+  end
+  
+  get '/:filename' do
+    params[:filename].slice!('.haml')
+    haml params[:filename].to_sym
   end
 end
