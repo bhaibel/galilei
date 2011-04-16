c89f77e93c270eaad241e36f3b597fb8a803fe2a
Betsy Haibel <betsy.haibel@gmail.com>
2011-04-16 14:21:26 -0400

    a starting to-do list

--- spec/galilei/helpers_spec.rb
+++ spec/galilei/helpers_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Galilei::Helpers do
-  pending 'everything'
+  it 'tests unknown methods to see if Faker responds to them'
+  
+  shared_examples_for "any text-generating method" do
+    it 'determines the correct range when passed a symbol'
+    it 'randomly selects a count within a range when passed a range'
+    it 'generates text'
+  end
 end
