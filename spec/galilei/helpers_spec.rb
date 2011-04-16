7a1150a60630b1994db576d91b19f49c930ffd86
Betsy Haibel <betsy.haibel@gmail.com>
2011-04-16 15:37:05 -0400

    fleshing out words spec more

--- spec/galilei/helpers_spec.rb
+++ spec/galilei/helpers_spec.rb
@@ -8,7 +8,6 @@ describe Galilei::Helpers do
   let(:tester) { TestClass.new }
   
   it 'tests unknown methods to see if Faker responds to them' do
-    pending "yes, RSpec, I know 'some_random_undefined_method_name' is undefined. That's the *point.*"
     Faker.constants.each do |const_name|
       const = Faker.const_get(const_name)
       if const.is_a? Class # we don't care about VERSION
@@ -17,6 +16,30 @@ describe Galilei::Helpers do
     end
     
     tester.some_random_undefined_method_name
+  end
+  
+  describe "#words" do
+    it 'generates text' do
+      tester.words.should be_an_instance_of(String)
+    end
+    
+    it 'generates a specific number of words when given a specific number' do
+      tester.words(3).split(' ').length.should == 3
+    end
+    
+    it 'generates a random number of words when given a range (but a number within the range)' do
+      different_numbers = false
+      seed_number = tester.words(1..20).split(' ').length
+      10.times do
+        x = tester.words(1..20).split(' ').length
+        if !different_numbers && x != seed_number
+          different_numbers = true
+        end
+        x.should be <= 20
+        x.should be >= 1
+      end
+      different_numbers.should be_true
+    end
   end
   
   shared_examples_for "any text-generating method" do
