fe3acf0c6347ea9ff51d2077e01e69806167da6b
Betsy Haibel <betsy.haibel@gmail.com>
2011-04-16 22:30:52 -0400

    paragraphs delimit properly!

--- lib/galilei/helpers.rb
+++ lib/galilei/helpers.rb
@@ -1,8 +1,20 @@
 module Galilei
   module Helpers
-    def method_missing(method_name, *args)
+    def paragraphs(*args)
       length = args.shift
-      
+      options = args.empty? ? {} : args.shift
+      generic(:paragraphs, length, options.merge({
+        :separator => '</p><p>',
+        :begin => '<p>',
+        :end => '</p>'}))
+    end
+        
+    def method_missing(method_name, *args)
+      generic(method_name, args.shift, (args.empty? ? {} : args.shift))
+    end
+    
+  private
+    def generic(method_name, length, options = {})
       # length can be a symbol, a range, an integer, or a fixnum. In the former two cases we need to convert it to something faker can use.
       
       # so first we translate symbols to ranges
@@ -18,21 +30,22 @@ module Galilei
       end
       
       #then we translate ranges to actual numbers
-      if length.class == Range
+      if length.is_a? Range
         length = rand(length.last - length.first) + length.first
       end
       
+      separator = options[:separator] || ' '
       Faker.constants.each do |faker_name|
         faker = Faker.const_get(faker_name)
         if faker.respond_to? method_name
           if length
-            return add_whitespace_if_array(faker.send method_name, length)
+            return add_whitespace_if_array(faker.send(method_name, length), separator)
           else
-            return add_whitespace_if_array(faker.send method_name)
+            return add_whitespace_if_array(faker.send(method_name), separator)
           end
         end
       end
-    end
+    end 
     
     def add_whitespace_if_array(possible_array, separator = ' ')
       possible_array.is_a?(Array) ? possible_array.join(separator) : possible_array
--- spec/galilei/helpers_spec.rb
+++ spec/galilei/helpers_spec.rb
@@ -18,17 +18,24 @@ describe Galilei::Helpers do
     tester.some_random_undefined_method_name
   end
   
-  def abstract_random_test(method_name, min = 1, max = 20, symbol = nil)
-    param = symbol || (min..max)
+  def abstract_random_test(method_name, options = {})
+    options = {
+      :min => 1,
+      :max => 20,
+      :delimiter => ' '
+    }.merge options
+    param = options[:symbol] || (options[:min]..options[:max])
     different_numbers = false
-    seed_number = tester.send(method_name, param).split(' ').length
-    ((max - min) * 2).times do
-      x = tester.send(method_name, param).split(' ').length
+    seed_number = tester.send(method_name, param).split(options[:delimiter]).length
+    ((options[:max] - options[:min]) * 2).times do
+      y = tester.send(method_name, param).split(options[:delimiter])
+      #raise y.inspect
+      x = y.length
       if !different_numbers && x != seed_number
         different_numbers = true
       end
-      x.should be <= max
-      x.should be >= min
+      x.should be <= options[:max]
+      x.should be >= options[:min]
     end
     different_numbers.should be_true
   end
@@ -47,17 +54,39 @@ describe Galilei::Helpers do
     end
     
     it 'generates between 1 and 5 words when passed :short' do
-      abstract_random_test :words, 1, 5, :short
+      abstract_random_test :words, :min => 1, :max => 5, :symbol => :short
     end
 
     it 'generates between 6 and 12 words when passed :medium' do
-      abstract_random_test :words, 6, 12, :medium
+      abstract_random_test :words, :min => 6, :max => 12, :symbol => :medium
     end
 
     it 'generates between 13 and 22 words when passed :long' do
-      abstract_random_test :words, 13, 22, :long
+      abstract_random_test :words, :min => 13, :max => 22, :symbol => :long
     end
   end
+
+  describe "#paragraphs" do
+    it 'generates text' do
+      tester.paragraphs.should be_an_instance_of(String)
+    end
+    
+      it 'generates a random number of paragraphs when given a range (but a number within the range)' do
+        abstract_random_test :paragraphs, :delimiter => '</p><p>'
+      end
+
+      it 'generates between 1 and 5 paragraphs when passed :short' do
+        abstract_random_test :paragraphs, :min => 1, :max => 5, :symbol => :short, :delimiter => '</p><p>'
+      end
+
+      it 'generates between 6 and 12 paragraphs when passed :medium' do
+        abstract_random_test :paragraphs, :min => 6, :max => 12, :symbol => :medium, :delimiter => '</p><p>'
+      end
+
+      it 'generates between 13 and 22 paragraphs when passed :long' do
+        abstract_random_test :paragraphs, :min => 13, :max => 22, :symbol => :long, :delimiter => '</p><p>'
+      end
+    end
   
   shared_examples_for "any text-generating method" do
     it 'determines the correct range when passed a symbol'
